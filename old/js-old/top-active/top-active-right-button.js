d43153e3c4ea17555c46473d4c8131fcd27a9d84
andrewherring <aherrin6@uwo.ca>
2022-03-02 10:13:05 -0700

    deleted unused files

--- old/js-old/top-active/top-active-right-button.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("rbttn").addEventListener("click",
-  function(){ location.href='./cyber/cyber.html'; });
-
-document.getElementById("rbttn").addEventListener("mouseover", function () {
-  bChange( "rbttn", rContent.rA, 1, 'var(--turq)'); });
-
-document.getElementById("rbttn").addEventListener("mouseout", function () {
-  bChange( "rbttn", rContent.rI, 0.6, 'white' ); });
